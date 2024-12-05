356af8714b4a1f2b4e34324f9777c5556209595b
mohitbeniwal77 <mohitbeniwal2025@gmail.com>
2024-12-05 17:42:13 +0530

    Hy bye code

---
+++ A.java
@@ -0,0 +1,15 @@
+class A
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+	}
+}
+
+class B
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Bye");
+	}
+}
