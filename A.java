7823e71cc3fb00fa93e737df9bff396c0764b2db
mohitbeniwal77 <mohitbeniwal2025@gmail.com>
2024-12-05 18:00:57 +0530

    bug fixes

--- A.java
+++ A.java
@@ -2,14 +2,15 @@ class A
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello");
+		System.out.println("Hello Hello");
 	}
 }
 
-class B
+class Welcome
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Bye");
+		System.out.println("Welcome");
+
 	}
 }
---
+++ Automation.java
@@ -0,0 +1,8 @@
+class Automation
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Programing");
+
+	}
+}
